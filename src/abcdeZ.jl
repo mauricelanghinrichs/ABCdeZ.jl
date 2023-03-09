e88069b8e01eecc43cf8c55fa967007fc0db2f33
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-03-09 13:44:59 +0100

    init update

--- src/abcdeZ.jl
+++ src/abcdeZ.jl
@@ -4,10 +4,10 @@ module abcdeZ
     using Distributions
     using FLoops
 
-    include("abcde_types.jl")
-
     include("abcde_priors.jl")
     export Factored
+
+    include("abcde_types.jl")
 
     include("abcde_mc.jl")
     export abcdemc!
