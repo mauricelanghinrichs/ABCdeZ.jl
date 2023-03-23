09646863585dc6b7736e7e57cbdc31f80073a301
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-03-23 21:53:00 +0100

    base | statsbase dep and init file

--- src/abcdeZ.jl
+++ src/abcdeZ.jl
@@ -2,12 +2,14 @@ module abcdeZ
 
     using Random
     using Distributions
+    using StatsBase
     using FLoops
 
     include("abcde_priors.jl")
     export Factored
 
     include("abcde_types.jl")
+    include("abcde_init.jl")
 
     include("abcde_mc.jl")
     export abcdemc!
