f25920bb399c56969af93725564917ff5b880137
mauricelanghinrichs <m.langhinrichs@icloud.com>
2023-03-31 13:15:27 +0200

    priors | docstrings

--- src/abcdez_priors.jl
+++ src/abcdez_priors.jl
@@ -4,16 +4,25 @@ struct MixedSupport <: ValueSupport end
 
 """
     Factored{N} <: Distribution{Multivariate, MixedSupport}
-a `Distribution` type that can be used to combine multiple `UnivariateDistribution`'s and sample from them.
-Example: it can be used as `prior = Factored(Normal(0,1), Uniform(-1,1))`
+
+A `Distribution` type that can be used to combine multiple `UnivariateDistribution`'s (independently).
+
+# Examples
+```julia-repl
+julia> prior = Factored(Normal(0, 1), Uniform(-1, 1))
+Factored{2}(
+p: (Normal{Float64}(μ=0.0, σ=1.0), Uniform{Float64}(a=-1.0, b=1.0))
+)
+```
 """
 struct Factored{N} <: Distribution{Multivariate,MixedSupport}
     p::NTuple{N,UnivariateDistribution}
     Factored(args::UnivariateDistribution...) = new{length(args)}(args)
 end
 """
-    pdf(d::Factored, x) = begin
-Function to evaluate the pdf of a `Factored` distribution object
+    pdf(d::Factored, x)
+
+Function to evaluate the pdf of a `Factored` distribution object.
 """
 function pdf(d::Factored{N}, x) where {N}
     s = pdf(d.p[1], x[1])
@@ -24,8 +33,9 @@ function pdf(d::Factored{N}, x) where {N}
 end
 
 """
-    logpdf(d::Factored, x) = begin
-Function to evaluate the logpdf of a `Factored` distribution object
+    logpdf(d::Factored, x)
+
+Function to evaluate the logpdf of a `Factored` distribution object.
 """
 function logpdf(d::Factored{N}, x) where {N}
     s = logpdf(d.p[1], x[1])
@@ -37,13 +47,15 @@ end
 
 """
     rand(rng::AbstractRNG, factoreddist::Factored)
-function to sample one element from a `Factored` object
+
+Function to sample one element from a `Factored` object.
 """
 rand(rng::AbstractRNG, factoreddist::Factored{N}) where {N} =
     ntuple(i -> rand(rng, factoreddist.p[i]), Val(N))
 
 """
-    length(p::Factored) = begin
-returns the number of distributions contained in `p`.
+    length(p::Factored)
+
+Returns the number of distributions contained in `p`.
 """
 length(p::Factored{N}) where {N} = N
